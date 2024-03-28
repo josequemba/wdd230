39095b854d1a66cfcf4b76b2f8b463338628c8d4
josequemba <joseeliud33@gmail.com>
2024-03-28 04:15:20 +0100

    some

---
+++ chamber/script/directory.js
@@ -0,0 +1,87 @@
+//last mofification
+document.addEventListener("DOMContentLoaded", function() {
+    const lastModified = document.lastModified;
+    document.getElementById("lastModified").textContent = lastModified;
+});
+
+//humburger menu
+menuHum();
+
+function menuHum () {
+    const buttonMenus = document.getElementsByClassName("hamburger-menu");
+    const navMenus = document.getElementsByClassName("nav-box");
+    
+    for (let i = 0; i < buttonMenus.length; i++) {
+        buttonMenus[i].addEventListener('click', () => {
+            for (let j = 0; j < navMenus.length; j++) {
+                navMenus[j].classList.toggle('open');
+            }
+            buttonMenus[i].classList.toggle('open');
+        });
+    }
+}
+
+//Getting current year
+displayyear();
+
+function displayyear () {
+    const currentDate = new Date();
+    const currentYear = currentDate.getFullYear();
+    
+    document.getElementById("year").textContent = currentDate.getUTCFullYear();
+}
+
+// Load members from JSON
+let isGridView = true;
+const buttonView = document.getElementById('button-view');
+const urlMembers = 'data/members.json';
+const membersContainer = document.getElementById('members-container');
+
+function updateButton () {
+    if (isGridView) {
+        buttonView.innerHTML = 'List';
+    } else {
+        buttonView.innerHTML = 'Grid';
+    }
+}
+updateButton();
+
+async function apiFetch(link) {
+    try {
+        const response = await fetch(link);
+        if (response.ok) {
+            const data = await response.json();
+            displayMembers(data.members);
+            //console.table(data);
+        }
+    } catch (error) {
+        console.error(error);
+    }
+}
+apiFetch(urlMembers);
+
+function displayMembers(data) {
+    membersContainer.innerHTML = ''; // Clear previous content
+    data.forEach(member => {
+      const memberElement = document.createElement('div');
+      memberElement.classList.add('member');
+      memberElement.innerHTML = `
+        <img src="images/${member.image}" alt="${member.name}">
+        <h2>${member.name}</h2>
+        <p>${member.address}</p>
+        <p>Phone: ${member.phone}</p>
+        <p>Website: <a href="${member.website}" target="_blank">${member.website}</a></p>
+        <p>Membership Level: ${member.membership_level}</p>
+        <p>${member.other_information}</p>
+      `;
+      membersContainer.appendChild(memberElement);
+    });
+};
+
+
+buttonView.addEventListener('click', ()=> {
+    isGridView = !isGridView;
+    updateButton ();
+    membersContainer.classList.toggle('grid-view');
+    membersContainer.classList.toggle('list-view');
+});
